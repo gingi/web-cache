cb5551eb112b4a4f46a956c6a52380b65763c541
Shiran Pasternak <shiranpasternak@gmail.com>
2013-01-17 18:18:27 -0500

    Using Redis for LRU cache.

--- web-cache.js
+++ web-cache.js
@@ -1,18 +1,56 @@
-var redis    = require('redis').createClient();
+var Redis = require('redis');
 
-function cacheResponse(res, fn, key) {
+var ONE_DAY = 86400;
+
+var DEFAULTS = {
+    path:     '/',
+    maxItems: 5000,
+    expire:   ONE_DAY,
+    prefix:   "web-cache"
+};
+
+function cacheResponse(res, fn, key, cache) {
     return function (body) {
+        // Will not cache empty values
         if (body) {
-            var value = JSON.stringify(body);
-            redis.set(key, value);
+            cache.lruSet(key, body);
         }
         fn.call(res, body);
     }
 }
 
+function initializeParams(inParams) {
+    var params = {};
+    for (var property in DEFAULTS) { params[property] = DEFAULTS[property]; }
+    for (var property in inParams) { params[property] = inParams[property]; }
+    return params;
+}
+
+function lruDecorate(cache, params) {
+    cache.lruGet = function (key, fn) {
+        cache.hget(params.prefix, key, function (err, reply) {
+            if (reply) {
+                reply = JSON.parse(reply);
+                cache.lruSet(key, reply);
+            }
+            fn(err, reply);
+        });
+    };
+    cache.lruSet = function (key, val, fn) {
+        var retval = cache.hset(params.prefix, key, JSON.stringify(data), fn);
+        cache.expire(params.prefix, key, params.expire);
+        return retval;
+    }
+}
+
 exports.middleware = function (params) {
-    params      = params      || {};
-    params.path = params.path || '/';
+    params = initializeParams(params);
+    
+    var cache = lruDecorate(Redis.createClient(params.port, params.host));
+    
+    if ("string" !== typeof params.prefix) {
+        throw new Error("'prefix' property must be a string");
+    }
     if (!params.path instanceof RegExp) {
         params.path = new RegExp("^" + params.path);
     }
@@ -31,15 +69,12 @@ exports.middleware = function (params) {
             }
         }
         res.contentType = 'application/json';
-        redis.get(req.url, function (err, reply) {
-            if (reply && reply !== "") {
-                var body = JSON.parse(reply);
-                if (body !== null && body !== {}) {
-                    return res.send(JSON.parse(reply));
-                }
+        
+        cache.lruGet(req.url, function (err, reply) {
+            if (reply) {
+                return res.send(body);
             }
-            var original = res.end;
-            res.end = cacheResponse(res, original, req.url);
+            res.end = cacheResponse(res, res.end, req.url, cache);
             next();
         });
     }
